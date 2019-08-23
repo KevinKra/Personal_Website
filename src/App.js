754580ac4c1d0e08a6f1a8ce12227e0430dd1ae5
Kevin Krato <kratok08@gmail.com>
2019-08-23 17:59:32 -0600

    Complete basic layout of project component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import InsetImage from "./components/InsetImage/InsetImage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="Project">
+        <div className="border-style"></div>
+        <header className="project-header">
+          <div className="header-skew">
+            <h1 className="project-title">PalettePicker</h1>
+            <a
+              href="https://github.com/KevinKra/PalettePicker-fe"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+          </div>
+        </header>
+        <section className="details-section">
+          <div className="details-paragraph">
+            <p>
+              Have a project you want to build but you're not sure about the
+              color palette? Use PalettePicker! PalettePicker lets you
+              personalize your color scheme by providing a wide variety of
+              styling options including hue selection, scheme options, and color
+              variations. Once you've locked down the perfect color combination,
+              save it and add it to a new project or a pre-existing project!
+            </p>
+          </div>
+          <div className="built-with">
+            <i className="fab fa-2x fa-react"></i>
+            <i className="fab fa-2x fa-js"></i>
+            <i className="fab fa-2x fa-sass"></i>
+            <i className="fab fa-2x fa-html5"></i>
+          </div>
+        </section>
+        <section className="preview-section">
+          <h2>PalettePicker</h2>
+          <InsetImage />
+        </section>
+      </section>
     </div>
   );
 }
