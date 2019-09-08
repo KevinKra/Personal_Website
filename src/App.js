876fd69b8a3d6ea1c1f2325f7e2790197803c0d5
Turing School <kratok08@gmail.com>
2019-09-08 11:34:05 -0600

    Update passed props to include deployURL

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ import imgMovieTracker from "./images/MovieTracker.jpg";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Project
         formatTitleLeft={true}
-        title="PalettePicker"
+        title='PalettePicker'
         image={imgPalettePicker}
-        githubURL="https://github.com/KevinKra/PalettePicker-fe"
+        githubURL='https://github.com/KevinKra/PalettePicker-fe'
+        deployURL='https://google.com'
         details={`Have a project you want to build but you're not sure about the color
         palette? Use PalettePicker! PalettePicker lets you personalize your
         color scheme by providing a wide variety of styling options
@@ -22,16 +23,18 @@ function App() {
       />
       <Project
         formatTitleLeft={false}
-        title="Light-Side"
+        title='Light-Side'
         image={imgLightSide}
-        githubURL="https://github.com/KevinKra/LightSide"
+        githubURL='https://github.com/KevinKra/LightSide'
+        deployURL='https://google.com'
         details={`LightSide is an application that uses the StarWars API (SWAPI) to present information related to the famous people, noteworthy planets, and popular vehicles, of the StarWars universe. As a user you have the ability to seamlessly sift through the data and favorite those cards that interest you and built up a collection that can be modified as you find other elements of the universe that you are fond of.`}
       />
       <Project
         formatTitleLeft={true}
-        title="Galleria"
+        title='Galleria'
         image={imgMovieTracker}
-        githubURL="https://github.com/KevinKra/Movies"
+        githubURL='https://github.com/KevinKra/Movies'
+        deployURL='https://google.com'
         details={`Galleria brings the movie-watching experience directly to its users by providing a visually appealing interactive design that both provides the ability to login to an account and favorite content based on your personal preferences. Search through various different genre and find the movies and shows that interest you the most!`}
       />
     </div>
