c6a4303f89228452d90af2fa17fede57f62d49b5
Kevin Krato <kratok08@gmail.com>
2019-09-17 16:00:14 -0600

    Dynamically animate in images at different intervals

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import dwarfOne from "../../images/artwork/dwarf_1.png";
 import dwarfTwo from "../../images/artwork/dwarf_2.png";
 import knightOne from "../../images/artwork/knight_1.png";
@@ -8,28 +8,45 @@ import guyOne from "../../images/artwork/guy_1.png";
 import cowboyOne from "../../images/artwork/cowboy_1.png";
 import "./Intro.scss";
 
-export default function Intro() {
-  return (
-    <section className="Intro">
-      <div className="blurb">
-        <h1>Kevin Krato</h1>
-        <p>Frontend Developer - Designer - Illustrator</p>
-      </div>
-      <img src={dwarfTwo} alt="A dwarf" className="image-1 slide-appear" />
-      <img src={dwarfOne} alt="A dwarf" className="image-2 slide-appear" />
-      <img src={knightOne} alt="A knight" className="image-3 slide-appear" />
-      <img src={faceOne} alt="A males face" className="image-5 slide-appear" />
-      <img
-        src={knightTwo}
-        alt="A knight in armor"
-        className="image-6 slide-appear"
-      />
-      <img
-        src={guyOne}
-        alt="A man with a hat"
-        className="image-7 slide-appear"
-      />
-      <img src={cowboyOne} alt="A cowboy" className="image-8 slide-appear" />
-    </section>
-  );
+export default class Intro extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <section className="Intro">
+        <div className="blurb">
+          <h1>Kevin Krato</h1>
+          <p>Frontend Developer - Designer - Illustrator</p>
+        </div>
+        <img src={dwarfTwo} alt="A dwarf" className="image-1 slide-appear-2" />
+        <img src={dwarfOne} alt="A dwarf" className="image-2 slide-appear-1" />
+        <img
+          src={knightOne}
+          alt="A knight"
+          className="image-3 slide-appear-2"
+        />
+        <img
+          src={faceOne}
+          alt="A males face"
+          className="image-5 slide-appear-1"
+        />
+        <img
+          src={knightTwo}
+          alt="A knight in armor"
+          className="image-6 slide-appear-4"
+        />
+        <img
+          src={guyOne}
+          alt="A man with a hat"
+          className="image-7 slide-appear-2"
+        />
+        <img
+          src={cowboyOne}
+          alt="A cowboy"
+          className="image-8 slide-appear-3"
+        />
+      </section>
+    );
+  }
 }
