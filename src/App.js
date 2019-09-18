c33e3c2b13b63c1528bf59d0634024ec2699124e
Kevin Krato <kratok08@gmail.com>
2019-09-18 15:16:51 -0600

    Add additional media queries for phones and resolve an overflow bug with the inset Image

--- src/App.js
+++ src/App.js
@@ -158,11 +158,33 @@ function App() {
           details={`Galleria brings the movie-watching experience directly to its users by providing a visually appealing interactive design that both provides the ability to login to an account and favorite content based on your personal preferences. Search through various different genre and find the movies and shows that interest you the most!`}
         />
       </section>
-      <footer className="PageFooter">
+      <section className="PageFooter">
         <div className="section-title-container">
           <h2 className="page-section-title">Reach out!</h2>
         </div>
-      </footer>
+        <div className="footer-form">
+          <form
+            className="email-form"
+            action="https://formspree.io/kevinkrato92@gmail.com"
+            method="POST"
+          >
+            <label htmlFor="name">Your name:</label>
+            <input type="text" name="name" placeholder="Name"></input>
+            <label htmlFor="name">Email:</label>
+            <input type="email" placeholder="Email" name="_replyto"></input>
+            <label htmlFor="message">Message:</label>
+            <textarea
+              name="message"
+              cols="30"
+              rows="10"
+              placeholder="Message"
+            ></textarea>
+            <button type="submit" value="Send">
+              Submit
+            </button>
+          </form>
+        </div>
+      </section>
     </div>
   );
 }
