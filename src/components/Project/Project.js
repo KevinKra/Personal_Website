a45f21c2b960ac13b77d0f36d7988ab1778b4e13
Kevin Krato <kratok08@gmail.com>
2019-09-20 08:56:15 -0600

    Refactor Project styles

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -10,28 +10,35 @@ export default function Project(props) {
       animateIn={!props.formatTitleLeft ? "fadeInLeft" : "fadeInRight"}
       animateOnce={true}
     >
-      <section className='Project'>
+      <section className="Project">
         {/* RIGHT FORMAT SETTINGS */}
         {!props.formatTitleLeft && (
-          <section className='preview-section' style={{ transform: "skew(5deg)" }}>
-            <header className='preview-header'>
+          <section
+            className="preview-section ps-right"
+            style={{ transform: "skew(5deg)" }}
+          >
+            <header className="preview-header">
               <h2>
                 {props.title}
                 <span> - Preview</span>
               </h2>
-              <div className='preview-icons'>
-                <a href={props.githubURL} target='_blank' rel='noopener noreferrer'>
-                  <i className='fab fa-2x fa-github'></i>
+              <div className="preview-icons">
+                <a
+                  href={props.githubURL}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fab fa-2x fa-github"></i>
                 </a>
-                <i className='fas fa-2x fa-arrow-circle-right'></i>
+                <i className="fas fa-2x fa-arrow-circle-right"></i>
               </div>
             </header>
             <InsetImage image={props.image} />
           </section>
         )}
-        <div className='border-style'></div>
+        <div className="border-style"></div>
         <header
-          className='project-header'
+          className="project-header"
           style={
             !props.formatTitleLeft
               ? { right: "2rem", transform: "skew(-20deg)" }
@@ -39,49 +46,67 @@ export default function Project(props) {
           }
         >
           <div
-            className='header-skew'
+            className="header-skew"
             style={
               !props.formatTitleLeft
                 ? { transform: "skew(20deg)" }
                 : { transform: "skew(-20deg)" }
             }
           >
-            <h1 className='project-title'>{props.title}</h1>
-            <a href={props.githubURL} target='_blank' rel='noopener noreferrer'>
+            <h1 className="project-title">{props.title}</h1>
+            <a
+              href={props.githubURL}
+              className="title-links"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               GitHub
             </a>
-            <a href={props.deployURL} target='_blank' rel='noopener noreferrer'>
+            <a
+              href={props.deployURL}
+              className="title-links"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Visit
             </a>
           </div>
         </header>
-        <section className='details-section'>
-          <div className='details-paragraph'>
-            <p>{props.details}</p>
-          </div>
-          <div
-            className='built-with'
-            style={!props.formatTitleLeft ? { right: "2rem" } : { left: "2rem" }}
-          >
-            <i className='fab fa-2x fa-react'></i>
-            <i className='fab fa-2x fa-js'></i>
-            <i className='fab fa-2x fa-sass'></i>
-            <i className='fab fa-2x fa-html5'></i>
-          </div>
+        <section
+          className={
+            !props.formatTitleLeft
+              ? "details-paragraph dp-right"
+              : "details-paragraph dp-left"
+          }
+        >
+          <p>{props.details}</p>
         </section>
+        <div
+          className="built-with"
+          style={!props.formatTitleLeft ? { right: "2rem" } : { left: "2rem" }}
+        >
+          <i className="fab fa-2x fa-react"></i>
+          <i className="fab fa-2x fa-js"></i>
+          <i className="fab fa-2x fa-sass"></i>
+          <i className="fab fa-2x fa-html5"></i>
+        </div>
         {/* LEFT FORMAT SETTINGS */}
         {props.formatTitleLeft && (
-          <section className='preview-section'>
-            <header className='preview-header'>
+          <section className="preview-section ps-left">
+            <header className="preview-header">
               <h2>
                 {props.title}
                 <span> - Preview</span>
               </h2>
-              <div className='preview-icons'>
-                <a href={props.githubURL} target='_blank' rel='noopener noreferrer'>
-                  <i className='fab fa-2x fa-github'></i>
+              <div className="preview-icons">
+                <a
+                  href={props.githubURL}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fab fa-2x fa-github"></i>
                 </a>
-                <i className='fas fa-2x fa-arrow-circle-right'></i>
+                <i className="fas fa-2x fa-arrow-circle-right"></i>
               </div>
             </header>
             <InsetImage image={props.image} />
